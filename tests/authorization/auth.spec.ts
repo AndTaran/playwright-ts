697f4722b89688d83c3aeb20ea4e2d656dac268e
AndTaran <skorp93ru@yandex.ru>
2024-07-15 17:06:36 +0300

    add steps to tests

--- tests/authorization/auth.spec.ts
+++ tests/authorization/auth.spec.ts
@@ -10,46 +10,71 @@ const errorAccountNotFound: string = "Аккаунт не найден";
 test.beforeEach(async ({ page }: { page: Page }) => {
 	const loginPageObj = new LoginPage(page);
 
-	await loginPageObj.goTo(url);
+	await test.step("Открытие страницы", async () => {
+		await loginPageObj.goTo(url);
+	});
 });
 
 test("Проверка отображения страницы регистрация/авторизация", async ({ page }: { page: Page }) => {
 	const loginPageObj = new LoginPage(page);
 
-	await expect(page).toHaveTitle("React App");
-	await expect(loginPageObj.mainTitle).toHaveText("Регистрация/Авторизация");
+	await test.step("Отображение заголовка", async () => {
+		await expect(loginPageObj.mainTitle).toHaveText("Регистрация/Авторизация");
+	});
 });
 
 test("Проверка отображения полей авторизации", async ({ page }: { page: Page }) => {
 	const loginPageObj = new LoginPage(page);
 
-	await expect(loginPageObj.email).toBeVisible();
-	await expect(loginPageObj.password).toBeVisible();
-	await expect(loginPageObj.authButton).toBeVisible();
+	await test.step("Отображение поля email", async () => {
+		await expect(loginPageObj.email).toBeVisible();
+	});
+
+	await test.step("Отображение поля пароля", async () => {
+		await expect(loginPageObj.password).toBeVisible();
+	});
+
+	await test.step("Отображение кнопки авторизации", async () => {
+		await expect(loginPageObj.authButton).toBeVisible();
+	});
 });
 
 test.describe("Негативные сценарии авторизации", () => {
 	test("Авторизация с незаполненными полями", async ({ page }: { page: Page }) => {
 		const loginPageObj = new LoginPage(page);
 
-		await loginPageObj.authButton.click();
-		await expect(loginPageObj.authError).toBeVisible();
-		await expect(loginPageObj.authError).toHaveText(errorFillEmailPassword);
+		await test.step("Нажатие на кнопку авторизации", async () => {
+			await loginPageObj.authButton.click();
+		});
+
+		await test.step("Отображение ошибки", async () => {
+			await expect(loginPageObj.authError).toBeVisible();
+			await expect(loginPageObj.authError).toHaveText(errorFillEmailPassword);
+		});
 	});
 
 	test("Неуспешная авторизация", async ({ page }: { page: Page }) => {
 		const loginPageObj = new LoginPage(page);
 
-		await loginPageObj.login(email, "12345");
+		await test.step("Авторизация от незарегистрированного пользователя", async () => {
+			await loginPageObj.login(email, "12345");
+		});
 
-		await expect(loginPageObj.authError).toBeVisible();
-		await expect(loginPageObj.authError).toHaveText(errorAccountNotFound);
+		await test.step("Отображение ошибки", async () => {
+			await expect(loginPageObj.authError).toBeVisible();
+			await expect(loginPageObj.authError).toHaveText(errorAccountNotFound);
+		});
 	});
 });
 
 test("Успешная авторизация", async ({ page }: { page: Page }) => {
 	const loginPageObj = new LoginPage(page);
 
-	await loginPageObj.login(email, password);
-	await expect(page.locator(".email")).toHaveText(email);
+	await test.step("Авторизация", async () => {
+		await loginPageObj.login(email, password);
+	});
+
+	await test.step("Отображение email авторизованного пользователя", async () => {
+		await expect(page.locator(".email")).toHaveText(email);
+	});
 });
--- tests/registration/reg.spec.ts
+++ tests/registration/reg.spec.ts
@@ -10,71 +10,140 @@ const password: string = "123456";
 test.beforeEach(async ({ page }) => {
 	const loginPageObj = new LoginPage(page);
 
-	await loginPageObj.goTo(url);
+	await test.step("Открытие страницы", async () => {
+		await loginPageObj.goTo(url);
+	});
 });
 
 test("Проверка полей регистрации", async ({ page }: { page: Page }) => {
 	const registrationPageObj = new RegistrationPage(page);
 
-	await expect(registrationPageObj.email).toBeVisible();
-	await expect(registrationPageObj.password).toBeVisible();
-	await expect(registrationPageObj.repeatPassword).toBeVisible();
-	await expect(registrationPageObj.registrationButton).toBeVisible();
+	await test.step("Отображение поля email", async () => {
+		await expect(registrationPageObj.email).toBeVisible();
+	});
+
+	await test.step("Отображение поля пароля", async () => {
+		await expect(registrationPageObj.password).toBeVisible();
+	});
+
+	await test.step("Отображение поля повторения пароля", async () => {
+		await expect(registrationPageObj.repeatPassword).toBeVisible();
+	});
+
+	await test.step("Отображение кнопки регистрации", async () => {
+		await expect(registrationPageObj.registrationButton).toBeVisible();
+	});
 });
 
 test.describe("Негативные сценарии регистрации", () => {
 	test("Регистрация с пустыми данными", async ({ page }: { page: Page }) => {
 		const registrationPageObj = new RegistrationPage(page);
 
-		await registrationPageObj.registrationButton.click();
-		await expect(registrationPageObj.registrationError).toBeVisible();
-		await expect(registrationPageObj.registrationError).toHaveText("Заполните все поля");
+		await test.step("Нажатие на кнопку регистрации", async () => {
+			await registrationPageObj.registrationButton.click();
+		});
+
+		await test.step("Отображение ошибки", async () => {
+			await expect(registrationPageObj.registrationError).toBeVisible();
+			await expect(registrationPageObj.registrationError).toHaveText("Заполните все поля");
+		});
 	});
 
 	test("Регистрация с некорректным паролем", async ({ page }: { page: Page }) => {
 		const registrationPageObj = new RegistrationPage(page);
 
-		await registrationPageObj.email.fill(email);
-		await registrationPageObj.password.fill("12345");
-		await registrationPageObj.repeatPassword.fill("12345");
-		await registrationPageObj.registrationButton.click();
+		await test.step("Заполнение поля email", async () => {
+			await registrationPageObj.email.fill(email);
+		});
 
-		await expect(registrationPageObj.registrationError).toBeVisible();
-		await expect(registrationPageObj.registrationError).toHaveText("Firebase: Password should be at least 6 characters (auth/weak-password).");
+		await test.step("Заполнение поля пароля", async () => {
+			await registrationPageObj.password.fill("12345");
+		});
+
+		await test.step("Заполнение поля повторения пароля", async () => {
+			await registrationPageObj.repeatPassword.fill("12345");
+		});
+
+		await test.step("Нажатие на кнопку регистрации", async () => {
+			await registrationPageObj.registrationButton.click();
+		});
+
+		await test.step("Отображение ошибки", async () => {
+			await expect(registrationPageObj.registrationError).toBeVisible();
+			await expect(registrationPageObj.registrationError).toHaveText("Firebase: Password should be at least 6 characters (auth/weak-password).");
+		});
 	});
 
 	test("Регистрация с некорректным email", async ({ page }: { page: Page }) => {
 		const registrationPageObj = new RegistrationPage(page);
 
-		await registrationPageObj.email.fill("test1");
-		await registrationPageObj.password.fill(password);
-		await registrationPageObj.repeatPassword.fill(password);
-		await registrationPageObj.registrationButton.click();
+		await test.step("Заполнение поля email", async () => {
+			await registrationPageObj.email.fill("test1");
+		});
 
-		await expect(registrationPageObj.registrationError).toBeVisible();
-		await expect(registrationPageObj.registrationError).toHaveText("Firebase: Error (auth/invalid-email).");
+		await test.step("Заполнение поля пароля", async () => {
+			await registrationPageObj.password.fill(password);
+		});
+
+		await test.step("Заполнение поля повторения пароля", async () => {
+			await registrationPageObj.repeatPassword.fill(password);
+		});
+
+		await test.step("Нажатие на кнопку регистрации", async () => {
+			await registrationPageObj.registrationButton.click();
+		});
+
+		await test.step("Отображение ошибки", async () => {
+			await expect(registrationPageObj.registrationError).toBeVisible();
+			await expect(registrationPageObj.registrationError).toHaveText("Firebase: Error (auth/invalid-email).");
+		});
 	});
 
 	test("Регистрация с существующим email", async ({ page }: { page: Page }) => {
 		const registrationPageObj = new RegistrationPage(page);
 
-		await registrationPageObj.email.fill(email);
-		await registrationPageObj.password.fill(password);
-		await registrationPageObj.repeatPassword.fill(password);
-		await registrationPageObj.registrationButton.click();
+		await test.step("Заполнение поля email", async () => {
+			await registrationPageObj.email.fill(email);
+		});
 
-		await expect(registrationPageObj.registrationError).toBeVisible();
-		await expect(registrationPageObj.registrationError).toHaveText("Firebase: Error (auth/email-already-in-use).");
+		await test.step("Заполнение поля пароля", async () => {
+			await registrationPageObj.password.fill(password);
+		});
+
+		await test.step("Заполнение поля повторения пароля", async () => {
+			await registrationPageObj.repeatPassword.fill(password);
+		});
+
+		await test.step("Нажатие на кнопку регистрации", async () => {
+			await registrationPageObj.registrationButton.click();
+		});
+
+		await test.step("Отображение ошибки", async () => {
+			await expect(registrationPageObj.registrationError).toBeVisible();
+			await expect(registrationPageObj.registrationError).toHaveText("Firebase: Error (auth/email-already-in-use).");
+		});
 	});
 });
 
 test("Успешная регистрация", async ({ page }) => {
 	const registrationPageObj = new RegistrationPage(page);
 
-	await registrationPageObj.email.fill(randomEmail);
-	await registrationPageObj.password.fill(password);
-	await registrationPageObj.repeatPassword.fill(password);
-	await registrationPageObj.registrationButton.click();
+	await test.step("Заполнение поля email", async () => {
+		await registrationPageObj.email.fill(randomEmail);
+	});
 
-	await expect(page.locator(".email")).toHaveText(randomEmail);
+	await test.step("Заполнение поля пароля", async () => {
+		await registrationPageObj.password.fill(password);
+	});
+
+	await test.step("Заполнение поля повторения пароля", async () => {
+		await registrationPageObj.repeatPassword.fill(password);
+	});
+	await test.step("Нажатие на кнопку регистрации", async () => {
+		await registrationPageObj.registrationButton.click();
+	});
+
+	await test.step("Отображение почты зарегистрированного пользователя", async () => {
+		await expect(page.locator(".email")).toHaveText(randomEmail);
+	});
 });
